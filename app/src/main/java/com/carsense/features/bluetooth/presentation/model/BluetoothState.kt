705f01ec5af8256b43f80857aff4c2c088a4a289
mironalin <alinmiron3@gmail.com>
2025-05-14 02:52:02 +0300

    Refactor: Enhance BluetoothState model with connectedDeviceAddress property
    - Added connectedDeviceAddress property to BluetoothState data class to store the address of the currently connected Bluetooth device.
    - Updated documentation to clarify the purpose of the BluetoothState class and its properties, improving code readability and maintainability.

--- app/src/main/java/com/carsense/features/bluetooth/presentation/model/BluetoothState.kt
+++ app/src/main/java/com/carsense/features/bluetooth/presentation/model/BluetoothState.kt
@@ -3,11 +3,18 @@ package com.carsense.features.bluetooth.presentation.model
 import com.carsense.features.bluetooth.domain.BluetoothDeviceDomain
 import com.carsense.features.obd2.presentation.model.MessageModel
 
+/**
+ * Represents the current state of the Bluetooth connection.
+ *
+ * This data class is used to store the current state of the Bluetooth connection. It is used to
+ * store the current state of the Bluetooth connection.
+ */
 data class BluetoothState(
     val scannedDevices: List<BluetoothDeviceDomain> = emptyList(),
     val pairedDevices: List<BluetoothDeviceDomain> = emptyList(),
     val isConnected: Boolean = false,
     val isConnecting: Boolean = false,
+    val connectedDeviceAddress: String? = null,
     val errorMessage: String? = null,
     val messages: List<MessageModel> = emptyList()
 )
