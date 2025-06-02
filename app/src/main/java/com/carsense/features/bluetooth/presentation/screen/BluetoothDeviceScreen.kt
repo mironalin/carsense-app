ea7e78d1a47abd428fb373174825959febb78d4e
mironalin <mironalin22@stud.ase.ro>
2025-06-02 03:01:35 +0300

    Refactor BluetoothDeviceScreen to use BackButton component
    - Replaced the navigation icon in TopAppBar with a custom BackButton for consistent UI across screens.
    - Cleaned up imports by removing unused icons, enhancing code clarity and maintainability.

--- app/src/main/java/com/carsense/features/bluetooth/presentation/screen/BluetoothDeviceScreen.kt
+++ app/src/main/java/com/carsense/features/bluetooth/presentation/screen/BluetoothDeviceScreen.kt
@@ -12,14 +12,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.outlined.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -30,6 +26,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.carsense.features.bluetooth.domain.BluetoothDeviceDomain
 import com.carsense.features.bluetooth.presentation.model.BluetoothState
+import com.carsense.features.welcome.presentation.components.BackButton
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -48,12 +45,9 @@ fun BluetoothDeviceScreen(
             TopAppBar(
                 title = { Text("Select a Device") },
                 navigationIcon = {
-                    IconButton(onClick = onBackPressed) {
-                        Icon(
-                            imageVector = Icons.AutoMirrored.Outlined.ArrowBack,
-                            contentDescription = "Go back"
-                        )
-                    }
+                    BackButton(onClick = {
+                        onBackPressed()
+                    })
                 }
             )
         }
