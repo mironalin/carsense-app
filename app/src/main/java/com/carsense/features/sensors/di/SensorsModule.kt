9fc72afd47055ef40d3223844f3dfcb1a9649a83
mironalin <mironalin22@stud.ase.ro>
2025-06-05 01:55:11 +0300

    Add SensorApiService provision to SensorsModule
    - Introduce a new method in SensorsModule to provide SensorApiService using Retrofit.
    - Enhance dependency injection for sensor-related API interactions, ensuring a singleton instance is used throughout the application.

--- app/src/main/java/com/carsense/features/sensors/di/SensorsModule.kt
+++ app/src/main/java/com/carsense/features/sensors/di/SensorsModule.kt
@@ -1,11 +1,14 @@
 package com.carsense.features.sensors.di
 
+import com.carsense.features.sensors.data.api.SensorApiService
 import com.carsense.features.sensors.data.repository.SensorRepositoryImpl
 import com.carsense.features.sensors.domain.repository.SensorRepository
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
 import javax.inject.Singleton
 
 @Module
@@ -15,4 +18,12 @@ abstract class SensorsModule {
     @Binds
     @Singleton
     abstract fun bindSensorRepository(sensorRepositoryImpl: SensorRepositoryImpl): SensorRepository
+
+    companion object {
+        @Provides
+        @Singleton
+        fun provideSensorApiService(retrofit: Retrofit): SensorApiService {
+            return retrofit.create(SensorApiService::class.java)
+        }
+    }
 }
